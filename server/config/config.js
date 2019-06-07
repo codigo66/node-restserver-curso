63bd9ce6a485cfc6427fa6b586eb9b9e3b7bad32
Miguel Lopez <mlopez@codigo66.com>
2019-06-07 09:22:32 -0600

    Variable entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cursoUdemy:0PSxIxbFdtnO4wNZ@codigo66-bhih1.mongodb.net/cafe';
+    //urlDB = 'mongodb+srv://cursoUdemy:0PSxIxbFdtnO4wNZ@codigo66-bhih1.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
